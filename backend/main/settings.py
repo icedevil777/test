d77ff57e555c4d5b44b82b4ca965013e4f9779d5
Icedevil777 <icedevil777228@mail.ru>
2025-04-20 21:16:15 +0300

    # add

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from polls.serializers import UserSerializer, GroupSerializer
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
+def index(request):
+    # Данные для передачи в шаблон
+    context = {
+        'name': 'Иван Иванов',
+        'age': 28,
+        'skills': ['Python', 'Django', 'HTML']
+    }
+    
+    # Рендеринг шаблона my_template.html с переданным контекстом
+    return render(request, 'polls/index.html', context)
